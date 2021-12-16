b8c4c8920b8faaf1c4d4a5f8e0080fd33fe1edde
Nathan-cloud <59859702+Nathan-cloud@users.noreply.github.com>
2021-12-16 02:49:45 -0800

    initial commit

--- js/scripts.js
+++ js/scripts.js
@@ -260,6 +260,100 @@
     });
   }
 
+  //Price Range Slider
+  if ($(".price-range-slider").length) {
+    $(".price-range-slider").slider({
+      range: true,
+      min: 0,
+      max: 90,
+      values: [0, 84],
+      slide: function (event, ui) {
+        $("input.property-amount").val(ui.values[0] + " - " + ui.values[1]);
+      },
+    });
+
+    $("input.property-amount").val(
+      $(".price-range-slider").slider("values", 0) +
+        " - $" +
+        $(".price-range-slider").slider("values", 1)
+    );
+  }
+
+  //Price Range Slider
+  if ($(".range-slider-one").length) {
+    $(".range-slider-one .range-slider").slider({
+      range: true,
+      min: 1900,
+      max: 2030,
+      values: [1923, 2023],
+      slide: function (event, ui) {
+        $(".range-slider-one .count").text(ui.values[0] + " - " + ui.values[1]);
+      },
+    });
+
+    $(".range-slider-one .count").text(
+      $(".range-slider").slider("values", 0) +
+        " - " +
+        $(".range-slider").slider("values", 1)
+    );
+  }
+
+  //Area Range Slider
+  if ($(".area-range-slider").length) {
+    $(".area-range-slider").slider({
+      range: true,
+      min: 0,
+      max: 100,
+      values: [0, 50],
+      slide: function (event, ui) {
+        $(".area-amount").text(ui.values[1]);
+      },
+    });
+
+    $(".area-amount").text($(".area-range-slider").slider("values", 1));
+  }
+  // accordion box
+  if ($(".accordion-box").length) {
+    $(".accordion-box").on("click", ".acc-btn", function () {
+      var outerBox = $(this).parents(".accordion-box");
+      var target = $(this).parents(".accordion");
+
+      if ($(this).hasClass("active") !== true) {
+        $(outerBox).find(".accordion .acc-btn").removeClass("active ");
+      }
+
+      if ($(this).next(".acc-content").is(":visible")) {
+        return false;
+      } else {
+        $(this).addClass("active");
+
+        $(outerBox).find(".accordion").children(".acc-content").slideUp(300);
+        target.addClass("active-block");
+        $(this).next(".acc-content").slideDown(300);
+      }
+    });
+  }
+
+  //Salary Range Slider
+  if ($(".salary-range-slider").length) {
+    $(".salary-range-slider").slider({
+      range: true,
+      min: 0,
+      max: 20000,
+      values: [0, 15000],
+      slide: function (event, ui) {
+        $(".salary-amount .min").text(ui.values[0]);
+        $(".salary-amount .max").text(ui.values[1]);
+      },
+    });
+
+    $(".salary-amount .min").text(
+      $(".salary-range-slider").slider("values", 0)
+    );
+    $(".salary-amount .max").text(
+      $(".salary-range-slider").slider("values", 1)
+    );
+  }
   $(".link-image-action").magnificPopup({
     type: "image",
   });
